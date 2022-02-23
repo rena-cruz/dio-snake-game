ff90b61adf7310685d4e79ac9d9f07e5ce3f91a5
Renata Cruz <rena-cruz@hotmail.com>
2022-02-23 16:32:58 -0300

    Defining snake movements

--- src/script.js
+++ src/script.js
@@ -6,18 +6,41 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function createBG() {
-    context.fillStyle = "Lightgreen";
+    context.fillStyle = "Lightgray";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function createSnake(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "gray";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
+
